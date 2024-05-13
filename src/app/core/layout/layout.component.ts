aa435a9e2c2b737a08140fb230b0e5a0ddf35d7c
Dharmen Shah <shhdharmen@gmail.com>
2024-05-13 14:46:11 +0530

    feat: pages

--- src/app/core/layout/layout.component.ts
+++ src/app/core/layout/layout.component.ts
@@ -8,6 +8,8 @@ import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-layout',
@@ -21,9 +23,12 @@ import { map, shareReplay } from 'rxjs/operators';
     MatListModule,
     MatIconModule,
     AsyncPipe,
+    RouterLink,
+    RouterLinkActive
   ]
 })
 export class LayoutComponent {
+  rootRoutes = routes.filter(r=>r.path);
   private breakpointObserver = inject(BreakpointObserver);
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
